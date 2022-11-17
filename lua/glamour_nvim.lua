76165b751fa071a3fb812f491c3c61b5260bcf86
Taylor Phillips <taylorscottphillips@gmail.com>
2022-11-16 23:00:01 -0600

    Update TypeScript syntax highlighting

--- lua/glamour_nvim.lua
+++ lua/glamour_nvim.lua
@@ -302,17 +302,6 @@ Group.new('jsxBraces', c.orange, c.none, none)
 Group.new('jsxComponentName', c.yellow, c.none, none)
 
 -- TypeScript
--- Group.new('typescriptBraces', c.aqua, c.none, none)
--- Group.new('typescriptEndColons', c.aqua, c.none, none)
--- Group.new('typescriptExceptions', c.red, c.none, none)
--- Group.new('typescriptFuncKeyword', c.purple, c.none, italic)
--- Group.new('typescriptGlobalObjects', c.yellow, c.none, none)
--- Group.new('typescriptLabel', c.aqua, c.none, none)
--- Group.new('typescriptParens', c.aqua, c.none, none)
-Group.new('typescriptReserved', c.aqua, c.none, none)
--- Group.new('typescriptStorageClass', c.purple, c.none, italic)
--- Group.new('typescriptType', c.yellow, c.none, none)
-
 Group.new('typescriptAliasKeyword', c.none, c.none, none)
 Group.new('typescriptAmbientDeclaration', c.aqua, c.none, none)
 Group.new('typescriptAsyncFuncKeyword', c.red, c.none, none)
